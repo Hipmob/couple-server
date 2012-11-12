b88a9cb94c07e1c19ab74fd52f61be20acbdd536
fomojola <oluseyi@alum.mit.edu>
2012-11-11 23:03:06 -0500

    Fixed up the login to also send any existing partner info.

--- login.js
+++ login.js
@@ -44,21 +44,74 @@ function handler()
 			bcrypt.compare(request.body.password, results.rows[0].password, function(err, res) {
 			    if(res){
 				// got it: send the guid back
-				hstore.parse(results.rows[0].details, function(result){
-				    var responsedata = {'guid': results.rows[0].guid, 'name': results.rows[0].first_name };
-				    if('partner' in result) responsedata['partner'] = result['partner'];
-				    response.end(JSON.stringify(responsedata));
-				});
+				var rowid = results.rows[0].id, responsedata = {'guid': results.rows[0].guid, 'name': results.rows[0].first_name };
+
+				var update_last_login = function(id){
+				    // update the last_login time
+				    client.query("BEGIN");
+				    client.query('UPDATE app_person SET last_login = CURRENT_TIMESTAMP WHERE id = $1', [id], function(err2, results2){
+					if(err2){
+					    console.log("[login] a database error occured ["+err+"]");
+					    return;
+					}
+					client.query("COMMIT", function() {
+					});
+				    });
+				};
 				
-				// update the last_login time
-				client.query("BEGIN");
-				client.query('UPDATE app_person SET last_login = CURRENT_TIMESTAMP WHERE id = $1', [results.rows[0].id], function(err2, results2){
-				    if(err2){
-					console.log("[login] a database error occured ["+err+"]");
+				// check if we have an outstanding partnership
+				client.query("SELECT guid, requester_email, requester_guid, requested_guid, email FROM app_request where (requester_guid = $1 OR requested_guid = $1) AND is_active = 'T'", [results.rows[0].guid], function(err, requestinfo){
+				    if(err){
+					console.error("[login.process;2]:"+err);
+					console.error(err.stack);
+					response.end(JSON.stringify({ 'error': 'A database error occured: please try again later.' }));
 					return;
 				    }
-				    client.query("COMMIT", function() {
-				    });
+				    
+				    // pull out the row
+				    if("rows" in requestinfo && requestinfo.rows.length > 0){
+					// add the appropriate fields
+					var partnerrow = requestinfo.rows[0];
+					
+					var complete = function(errc, pinfo){
+					    if(errc){
+						console.error("[login.process;2]:"+errc);
+						console.error(errc.stack);
+						response.end(JSON.stringify({ 'error': 'A database error occured: please try again later.' }));
+						return;
+					    }
+					    
+					    if("rows" in pinfo && pinfo.rows.length > 0){
+						// and, fill it in
+						responsedata['partnerName'] = pinfo.rows[0].name;
+					    }
+					    response.end(JSON.stringify(responsedata));
+					    update_last_login(rowid);
+					};
+					
+					// fetch the name
+					responsedata['requestGuid'] = partnerrow.guid;
+					if(partnerrow.requester_email == request.body.email){
+					    responsedata['partnerEmail'] = partnerrow.email;
+					    responsedata['partnerGuid'] = partnerrow.requested_guid;
+					    client.query({
+						name: "get_partner_name",
+						text: "SELECT first_name AS name FROM app_person WHERE guid = $1",
+						values: [partnerrow.requested_guid]
+					    }, complete);
+					}else{
+					    responsedata['partnerEmail'] = partnerrow.requester_email;
+					    responsedata['partnerGuid'] = partnerrow.requester_guid;
+					    client.query({
+						name: "get_partner_name",
+						text: "SELECT first_name AS name FROM app_person WHERE guid = $1",
+						values: [partnerrow.requester_guid]
+					    }, complete);
+					}
+				    }else{
+					response.end(JSON.stringify(responsedata));
+					update_last_login(rowid);
+				    }
 				});
 			    }else if(err){
 				response.end(JSON.stringify({ 'error': 'An authentication error occured: please try again later.' }));
